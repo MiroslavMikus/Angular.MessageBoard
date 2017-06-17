4da485394566dceb350c939e239a31b82a1d667e
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-06-17 20:57:50 +0200

    Add Put Request

--- backend/backend/Controllers/MessagesController.cs
+++ backend/backend/Controllers/MessagesController.cs
@@ -12,21 +12,29 @@ namespace backend.Controllers
     [Route("api/Messages")]
     public class MessagesController : Controller
     {
+        static List<Message> _messages = new List<Message>()
+        {
+            new Message()
+            {
+                Owner = "MMI",
+                Text ="Some Text from backend"
+            },
+            new Message()
+            {
+                Owner = "ETR",
+                Text = "Also some test text"
+            }
+        };
+
         public IEnumerable<Message> Get()
         {
-            return new Message[]
-            {
-                new Message()
-                {
-                    Owner = "MMI",
-                    Text ="Some Text from backend"
-                },
-                new Message()
-                {
-                    Owner = "ETR",
-                    Text = "Also some test text"
-                }
-            };
+            return _messages;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Message a_message)
+        {
+            _messages.Add(a_message);
         }
     }
 }
