ba95c805b3121caf29917db56ac1ef526dbc6283
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-06-17 22:02:23 +0200

    Add event and ViewChild

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NewMessageComponent } from './new-message-component';
+import { MessageComponent } from './messages-component';
+
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>Message Board</h1>
-  <new-message></new-message>
+  <new-message (onPosted)="onPosted($event)"></new-message>
   <messages></messages>`,
 })
 
-export class AppComponent  {}
+export class AppComponent  {
+
+  @ViewChild(MessageComponent) messages : MessageComponent
+
+  onPosted(message){
+    this.messages.messages.push(message);
+  }
+}
--- frontend/src/app/new-message-component.ts
+++ frontend/src/app/new-message-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { WebService } from './web.service';
 
 @Component(
@@ -23,6 +23,8 @@ import { WebService } from './web.service';
 
 export class NewMessageComponent{
 
+    @Output() onPosted = new EventEmitter();
+
     constructor(private webService : WebService){}
 
     message = {
@@ -32,5 +34,6 @@ export class NewMessageComponent{
 
     post(){ 
         this.webService.postMessage(this.message);
+        this.onPosted.emit(this.message);
     }
 }
