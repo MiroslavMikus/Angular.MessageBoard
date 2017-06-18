c54bbe25b461007788f547cee3f8a506deff31d7
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-06-18 11:45:28 +0200

    Add password and email validation

--- frontend/src/app/register-component.ts
+++ frontend/src/app/register-component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -12,15 +12,22 @@ export class RegisterComponent  {
 
     constructor(private formBuilder : FormBuilder){
         this.form = formBuilder.group({
-            firstName: '',
-            lastName:'',
-            email:'',
-            password:'',
-            confirmPassword:''
-        })
+            firstName: ['', Validators.required ],
+            lastName:['', Validators.required ],
+            email:['', [Validators.required, Validators.email] ],
+            password:['', Validators.required ],
+            confirmPassword:['', Validators.required ]
+        }, { validator: matchingFileds('password','confirmPassword')})
     }
 
     onSubmit(){
-        console.log(this.form.value)
+        console.log(this.form.errors)
+    }
+}
+
+function matchingFileds(field1, field2){
+    return form =>{
+        if(form.controls[field1].value != form.controls[field2].value)
+        return { mismatchedFields: true};
     }
 }
