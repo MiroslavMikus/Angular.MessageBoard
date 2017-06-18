a0f1267256b6f54b5f4782a7daac8071e369d1bc
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-06-18 16:57:00 +0200

    Only logged in user can add message

--- frontend/src/app/home-component.ts
+++ frontend/src/app/home-component.ts
@@ -3,15 +3,18 @@ import { Component } from '@angular/core';
 import { NewMessageComponent } from './new-message-component';
 import { MessageComponent } from './messages-component';
 import { NavComponent } from './nav-component';
+import { AuthService } from './auth-service';
 
 
 @Component({
   selector: 'home',
   template: `
-  <new-message></new-message>
+  <new-message *ngIf="auth.isAuthenticated"></new-message>
   <messages></messages>`,
 })
 
 export class HomeComponent  {
+
+    constructor(private auth : AuthService){}
 
 }
